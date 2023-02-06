218285b8fe079323529da4b1a8109374e9f2bbac
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-06 21:03:47 +0100

    Correct codesmells by adding an if state and type useApi function

--- src/store/reducers/events/eventsReducer.ts
+++ src/store/reducers/events/eventsReducer.ts
@@ -1,18 +1,18 @@
 import { EventsStructure } from "../../../data/types";
 import { EventsActionType, loadEventsAction } from "../../actions/events/types";
 
+const eventsReducer = (
+  currentEvent: EventsStructure,
+  action: loadEventsAction
+): EventsStructure => {
+  let newEvents: EventsStructure;
 
-
-const eventsReducer = (currentEvent: EventsStructure, action: loadEventsAction): EventsStructure =>{
-  let newEvents: EventsStructure
-
-  switch(action.type){
-    case EventsActionType.loadEvents:
-      newEvents = [...(action as loadEventsAction).payload]
-      break;
-      default: newEvents=currentEvent
+  if (action.type === EventsActionType.loadEvents) {
+    newEvents = [...(action as loadEventsAction).payload];
+  } else {
+    newEvents = currentEvent;
   }
-  return newEvents
-} 
+  return newEvents;
+};
 
 export default eventsReducer;
