93963968e318722b00142c603685e0c2f9f1e925
Gaston82 <cabezaartaza@hotmail.com>
2023-02-06 20:45:17 +0100

    Add environment variables

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,14 +1,11 @@
 const useApi = () => {
   const loadEvents = async (keyword: string) => {
-    const API_KEY = `DxSOpYSZ4nVwPWsGOWdELH14DJA5EIYL`;
-    const API_BASE = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${API_KEY}&countryCode=ES`;
-
     try {
-      const result = await fetch(`${API_BASE}&keyword=${keyword}`);
+      const result = await fetch(process.env.REACT_APP_URL_API!);
       const events = await result.json();
       return events._embedded.events;
-    } catch (err) {
-      return null;
+    } catch (error: unknown) {
+      return (error as Error).message;
     }
   };
   return loadEvents;
