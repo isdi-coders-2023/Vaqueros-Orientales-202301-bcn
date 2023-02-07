ed006184e7c75a55c607dab845cd213635ec658a
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-07 15:20:26 +0100

    Modify Api interface

--- src/data/types.ts
+++ src/data/types.ts
@@ -2,13 +2,32 @@ export interface EventStructure {
   name: string;
   id: number;
   type: string;
-  image: [url: string];
-  address: { _embedded: [venues: { name: string }] };
-  city: { _embedded: [venues: { city: { name: string } }] };
-  country: { _embedded: [venues: { country: string }] };
-  price: [priceRanges: { min: number }];
-  date: { start: { localDate: number } };
-  time: { start: { localTime: number } };
+  url: string;
+  address: string;
+  city: string;
+  country: string;
+  price: number;
+  date: string;
+  time: string;
+}
+
+export interface ApiResponseStructure {
+  _embedded: {
+    events: [
+      {
+        name: string;
+        id: number;
+        type: string;
+        url: { images: { url: string } };
+        address: { _embedded: [venues: { name: string }] };
+        city: { _embedded: [venues: { city: { name: string } }] };
+        country: { _embedded: [venues: { country: string }] };
+        price: [priceRanges: { min: number }];
+        date: { start: { localDate: number } };
+        time: { start: { localTime: number } };
+      }
+    ];
+  };
 }
 
 export type EventsStructure = EventStructure[];
