f2155c6fcabfae1cdf93740d4a3db5989726a200
Gaston82 <cabezaartaza@hotmail.com>
2023-02-07 11:39:36 +0100

    Create EventsContextProvider

---
+++ src/store/contexts/events/EventsContextProvider.tsx
@@ -0,0 +1,18 @@
+import React, { useReducer } from "react";
+import eventsReducer from "../../reducers/events/eventsReducer";
+import EventsContext from "./EventsContext";
+
+interface EventsContextProviderProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+const EventsContextProvider = ({ children }: EventsContextProviderProps) => {
+  const [events, dispatch] = useReducer(eventsReducer, []);
+  return (
+    <EventsContext.Provider value={{ events, dispatch }}>
+      {children}
+    </EventsContext.Provider>
+  );
+};
+
+export default EventsContextProvider;
