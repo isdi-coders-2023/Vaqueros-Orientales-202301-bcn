85af228b84c05dde8697d8b4634b4fce1f98487a
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-07 12:49:10 +0100

    Modify and update Api interface

--- src/data/types.ts
+++ src/data/types.ts
@@ -2,12 +2,13 @@ export interface EventStructure {
   name: string;
   id: number;
   type: string;
-  image: string;
-  address: string;
-  price: number;
-  location: string;
-
-  time: string;
+  image: [url: string];
+  address: { _embedded: [venues: { name: string }] };
+  city: { _embedded: [venues: { city: { name: string } }] };
+  country: { _embedded: [venues: { country: string }] };
+  price: [priceRanges: { min: number }];
+  date: { start: { localDate: number } };
+  time: { start: { localTime: number } };
 }
 
 export type EventsStructure = EventStructure[];
