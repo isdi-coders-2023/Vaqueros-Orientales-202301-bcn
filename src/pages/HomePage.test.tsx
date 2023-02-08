eee27bf2bb3aa23e67b30d31bcfbe2b4db965bea
Gaston82 <cabezaartaza@hotmail.com>
2023-02-08 17:58:31 +0100

    Remove Home page duplicate

--- src/pages/HomePage.test.tsx
+++ src/pages/HomePage.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import Home from "./HomePage";
+import HomePage from "./HomePage";
 
 describe("Given the Home page", () => {
   describe("When it renders", () => {
     test("Then it should the text 'Home'", () => {
-      render(<Home />);
+      render(<HomePage />);
       const title = screen.getByRole("heading");
       expect(title).toBeInTheDocument();
     });
