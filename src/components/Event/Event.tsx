f4ae578f5397e6f9a417cfe8877af17073b6f057
David Botello <dabocher@gmail.com>
2023-02-08 11:15:05 +0100

    Fix component naming and properties naming and add another test

---
+++ src/components/Event/Event.tsx
@@ -0,0 +1,48 @@
+import { EventStructure } from "../../data/types";
+import EventStyled from "./EventStyled";
+
+interface EventProps {
+  event: EventStructure;
+}
+
+const Event = ({
+  event: {
+    name,
+    images: [{ url }],
+    _embedded: {
+      venues: [
+        {
+          city: { name: cityName },
+        },
+      ],
+    },
+  },
+}: EventProps): JSX.Element => {
+  return (
+    <EventStyled className="event">
+      <div className="card">
+        <button className="card__button-edit">*</button>
+        <div className="card__container-image">
+          <img
+            src={url}
+            alt="2 quarterbacks between the superbowl logo"
+            width="280"
+            className="card__image"
+          />
+        </div>
+        <div className="card__info">
+          <div className="container-info-city">
+            <span className="card__info-city">{cityName}</span>
+            <span className="card__info-date">13 Feb, 2023</span>
+          </div>
+          <div className="container-info-title">
+            <h2 className="card__info-title">{name}</h2>
+            <button className="card__button-fav">👍</button>
+          </div>
+        </div>
+      </div>
+    </EventStyled>
+  );
+};
+
+export default Event;
