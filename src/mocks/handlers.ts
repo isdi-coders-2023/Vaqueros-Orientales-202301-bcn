b823b27806436d139358d2445f30b7bcbd8f9be2
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-09 10:51:14 +0100

    Modify handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,19 +1,34 @@
 import { rest } from "msw";
 
-export const handlers = [
-  rest.get(
-    "https://app.ticketmaster.com/discovery/v2/events.json",
+const apiKey = `apikey=${process.env.REACT_APP_API_KEY}`;
+const url = `${process.env.REACT_APP_URL_API!}${apiKey}`;
 
-    (request, response, context) => {
-      const urlParameters = request.url.searchParams.getAll(
-        "?apikey=DxSOpYSZ4nVwPWsGOWdELH14DJA5EIYL&countryCode=ES"
-      );
-      return response(
-        context.status(200),
-        context.json({
-          urlParameters,
-        })
-      );
-    }
-  ),
+export const handlers = [
+  rest.get(`${url}`, (request, response, context) => {
+    return response(
+      context.status(200),
+      context.json({
+        _embedded: {
+          events: {
+            name: "SuperBowl",
+            id: "0",
+            type: "",
+            images: [{ url: "" }],
+            _embedded: {
+              venues: [
+                {
+                  name: "",
+                  city: { name: "Phoenix" },
+                  country: { name: "" },
+                  address: { line1: "" },
+                },
+              ],
+            },
+            priceRanges: [{ min: 0 }],
+            dates: { start: { localDate: "", localTime: "" } },
+          },
+        },
+      })
+    );
+  }),
 ];
