b01c7e6df576ef4c9fb07713c14dffe877a0da48
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-10 17:49:33 +0100

    Modify naming for eventsActionsCreators and delete duplicated

--- src/store/actions/events/eventsActionCreators.test.ts
+++ src/store/actions/events/eventsActionCreators.test.ts
@@ -1,6 +1,9 @@
-import { EventsStructure } from "../../../data/types";
-import { loadEventsActionCreator } from "./EventsActionsCreator";
-import { EventsActionType, LoadEventsAction } from "./types";
+import { EventsStructure, EventStructure } from "../../../data/types";
+import {
+  loadEventsActionCreator,
+  loadEventActionCreator,
+} from "./eventsActionsCreators";
+import { EventsActionType, LoadEventsAction, LoadEventAction } from "./types";
 
 describe("Given a loadEventsAction function", () => {
   describe("When it receives a list of two events", () => {
@@ -65,6 +68,46 @@ describe("Given a loadEventsAction function", () => {
       const loadEventsAction = loadEventsActionCreator(events);
 
       expect(loadEventsAction).toStrictEqual(expectedLoadEventsAction);
+    });
+  });
+});
+
+describe("Given a loadEventAction", () => {
+  describe("When it recieves an event", () => {
+    test("Then it should return an action with type 'loadEvent' and one event as a payload", () => {
+      const event: EventStructure = {
+        name: "Lady Gaga",
+        id: "",
+        type: "",
+        images: [
+          { url: "", ratio: "" },
+          { url: "", ratio: "" },
+          { url: "", ratio: "" },
+          { url: "", ratio: "" },
+          { url: "", ratio: "" },
+        ],
+
+        _embedded: {
+          venues: [
+            {
+              name: "",
+              city: { name: "" },
+              country: { name: "" },
+              address: { line1: "" },
+            },
+          ],
+        },
+        priceRanges: [{ min: 12 }],
+        dates: { start: { localDate: "", localTime: "" } },
+      };
+      const expectedLoadEventAction: LoadEventAction = {
+        type: EventsActionType.loadEvent,
+        payload: event,
+      };
+
+      const loadEventAction = loadEventActionCreator(event);
+
+      expect(loadEventAction).toStrictEqual(expectedLoadEventAction);
     });
   });
 });
