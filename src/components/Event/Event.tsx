6ea60931cbd8cbaafab940f43997587cb8dfd491
Gaston82 <cabezaartaza@hotmail.com>
2023-02-11 16:14:47 +0100

    Add Link to Event component

--- src/components/Event/Event.tsx
+++ src/components/Event/Event.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { EventStructure } from "../../data/types";
 import EventStyled from "./EventStyled";
 
@@ -8,30 +9,32 @@ interface EventProps {
 const Event = ({ event }: EventProps): JSX.Element => {
   return (
     <EventStyled className="event">
-      <div className="card">
-        <div className="card__container-image">
-          <img
-            src={event.images[4].url}
-            alt={event.name}
-            width="280"
-            height="157"
-            className="card__image"
-          />
-        </div>
-        <div className="card__info">
-          <div className="container-info-city">
-            <span className="card__info-city">
-              {event._embedded.venues[0].city.name}
-            </span>
-            <span className="card__info-date">
-              {event.dates.start.localDate}
-            </span>
+      <Link to={`/detail/:${event.id}`}>
+        <div className="card">
+          <div className="card__container-image">
+            <img
+              src={event.images[4].url}
+              alt={event.name}
+              width="280"
+              height="157"
+              className="card__image"
+            />
           </div>
-          <div className="container-info-title">
-            <h2 className="card__info-title">{event.name}</h2>
+          <div className="card__info">
+            <div className="container-info-city">
+              <span className="card__info-city">
+                {event._embedded.venues[0].city.name}
+              </span>
+              <span className="card__info-date">
+                {event.dates.start.localDate}
+              </span>
+            </div>
+            <div className="container-info-title">
+              <h2 className="card__info-title">{event.name}</h2>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </EventStyled>
   );
 };
