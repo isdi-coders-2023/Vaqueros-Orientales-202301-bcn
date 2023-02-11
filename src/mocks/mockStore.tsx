281ff1da446d23ff286d5d581938ac99bcf7e253
Gaston82 <cabezaartaza@hotmail.com>
2023-02-11 21:51:16 +0100

    Test DetailPage image and heading

--- src/mocks/mockStore.tsx
+++ src/mocks/mockStore.tsx
@@ -10,7 +10,30 @@ export const mockStore = {
   store: {
     events: [] as EventsStructure,
     dispatch,
-    event: {} as EventStructure,
+    event: {
+      name: "SuperBowl",
+      id: "0",
+      type: "",
+      images: [
+        { url: "", ratio: "" },
+        { url: "", ratio: "" },
+        { url: "", ratio: "" },
+        { url: "", ratio: "" },
+        { url: "beyonce.jpg", ratio: "" },
+      ],
+      _embedded: {
+        venues: [
+          {
+            name: "",
+            city: { name: "Phoenix" },
+            country: { name: "" },
+            address: { line1: "" },
+          },
+        ],
+      },
+      priceRanges: [{ min: 0 }],
+      dates: { start: { localDate: "", localTime: "" } },
+    } as EventStructure,
     detailDistpatch,
   },
 };
--- src/pages/DetailPage/DetailPage.test.tsx
+++ src/pages/DetailPage/DetailPage.test.tsx
@@ -1,13 +1,34 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import MockContextProvider from "../../mocks/MockContextProvider";
+import { mockStore } from "../../mocks/mockStore";
 import DetailPage from "./DetailPage";
+
+const store = mockStore;
 
 describe("Given the DetailPage", () => {
   describe("When it renders", () => {
-    test("Then it should a headind with the text 'EventInfo'", () => {
-      const pageTitle = "EventInfo";
-      render(<DetailPage />);
-      const expectedResult = screen.getByRole("heading", { name: pageTitle });
+    test("Then it should shows an image", () => {
+      render(
+        <MockContextProvider mockStore={store}>
+          <DetailPage />
+        </MockContextProvider>,
+        { wrapper: BrowserRouter }
+      );
+      const expectedResult = screen.getByRole("img");
       expect(expectedResult).toBeInTheDocument();
+    });
+
+    test("Then it should shows a heading with the text 'EventInfo'", () => {
+      render(
+        <MockContextProvider mockStore={store}>
+          <DetailPage />
+        </MockContextProvider>,
+        { wrapper: BrowserRouter }
+      );
+      const pageTitle = "EventInfo";
+      const expectedTitle = screen.getByRole("heading", { name: pageTitle });
+      expect(expectedTitle).toBeInTheDocument();
     });
   });
 });
