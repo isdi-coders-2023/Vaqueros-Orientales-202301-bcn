9149eebf28fbc81b25281c360d9efe175d2ecac9
David Botello <dabocher@gmail.com>
2023-02-11 12:07:11 +0100

    Delete paths on App routes to fix problem with router navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import { RouterProvider } from "react-router-dom";
-import CreateEvent from "./pages/CreateEventPage/CreateEventPage";
-import HomePage from "./pages/HomePage/HomePage";
 import router from "./router/router";
 
 const App = (): JSX.Element => {
   return (
     <div className="container">
       <RouterProvider router={router} />
-      <HomePage />
-      <CreateEvent />
     </div>
   );
 };
