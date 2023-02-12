c670a1be99de14a79916bfbb62484b40a23d9bfb
Gaston82 <cabezaartaza@hotmail.com>
2023-02-12 11:45:51 +0100

    Add DetailPage tsx

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router";
-import Event from "../../components/Event/Event";
 import useApi from "../../hooks/useApi";
 import EventsContext from "../../store/contexts/events/EventsContext";
 
@@ -19,8 +18,31 @@ const DetailPage = () => {
     <>
       <header>
         <h1>EventInfo</h1>
-        <Event event={event} />
       </header>
+      {event && (
+        <div className="event-detail">
+          <img src={event.images[4].url} alt={event.name} />
+          <h2>{event.name}</h2>
+          <div className="event-detail__info">
+            <div>
+              <h3>Type of Event</h3>
+              <span>{event.type}</span>
+              <h3>Date</h3>
+              <span>{event.dates.start.localDate}</span>
+              <h3>Time</h3>
+              <span>{event.dates.start.localDate}</span>
+              <h3>Location</h3>
+              <span>{event._embedded.venues[0].city.name}</span>
+            </div>
+            <div>
+              <h3>Address</h3>
+              <span>{event._embedded.venues[0].address.line1}</span>
+              <h3>Price</h3>
+              <span>75 €</span>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
