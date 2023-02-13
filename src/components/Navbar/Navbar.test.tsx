e14f06c7aa5e7425b276976e8fd92d64bea556d7
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-13 15:50:00 +0100

    Refactor Navbar test

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -1,64 +1,29 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import NavigationBar from "./Navbar";
+import Navbar from "./Navbar";
 
 describe("Given a Navbar Component", () => {
-  describe("When it renders with a list of 3 icons", () => {
-    test("Then it should show 3 li elements", () => {
-      render(
-        <>
-          <BrowserRouter>
-            <NavigationBar />
-          </BrowserRouter>
-        </>
-      );
-
-      const info = screen.getAllByRole("listitem").length;
-
-      expect(info).toBe(3);
-    });
-  });
-
-  describe("When it renders with a list", () => {
-    test("Then it should show a element with ul tag", () => {
-      render(
-        <>
-          <BrowserRouter>
-            <NavigationBar />
-          </BrowserRouter>
-        </>
-      );
-
-      const info = screen.getByRole("list").innerHTML;
-
-      expect(info).toContain("<li");
-    });
-  });
-
   describe("When it renders", () => {
-    test("Then it shoul show a ul-list element", () => {
+    test("Then it should show a list of 3 icons", () => {
+      const numberOfIcons = 3;
+
       render(
-        <>
-          <BrowserRouter>
-            <NavigationBar />
-          </BrowserRouter>
-        </>
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
       );
+      const icons = screen.getAllByRole("listitem");
 
-      const info = screen.getByRole("list");
-
-      expect(info).toBeInTheDocument();
+      expect(icons).toHaveLength(numberOfIcons);
     });
   });
 
   describe("When it renders by click a link", () => {
     test("Then it should show one active link", () => {
       render(
-        <>
-          <BrowserRouter>
-            <NavigationBar />
-          </BrowserRouter>
-        </>
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
       );
       fireEvent.click(screen.getByRole("link", { name: "Create an Event" }));
       const event = screen.getByRole("link", {
